d224f7a11063ea60a0fc1bfaabf87164447b28d2
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2021-06-30 11:49:18 +0900

    Fix navi

--- app/services/docin/navi/imports_service.rb
+++ app/services/docin/navi/imports_service.rb
@@ -1,15 +1,2 @@
 class Docin::Navi::ImportsService < Core::Navi::BaseService
-  class Builder < Builder
-    def nodes
-      [root_node]
-    end
-
-    def root_node
-      Core::Navi::Node.new(
-        id: ROOT_ID,
-        title: '記事取込',
-        link_url: { current_node: ROOT_ID }
-      )
-    end
-  end
 end
