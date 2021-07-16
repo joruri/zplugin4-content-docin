248ac8e63675dd8a31107502803217a67908bc84
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2021-07-16 10:56:27 +0900

    Remove redundant inclusion of ActiveHash::Tree

--- app/models/docin/content/setting_menu.rb
+++ app/models/docin/content/setting_menu.rb
@@ -1,7 +1,4 @@
 class Docin::Content::SettingMenu < Cms::Content::SettingMenu
-  include ActiveHash::Tree
-  include ActiveHash::Tree::AddItem
-
   has_many :fields, class_name: 'Docin::Content::SettingField', foreign_key: :menu_id
 
   add_item :relation do
