4f9f17cce7447ff3cd5fc7b437489a0394fcb137
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2021-07-26 12:00:27 +0900

    Fix routes

--- config/modules/routes.rb
+++ config/modules/routes.rb
@@ -1,22 +1,13 @@
-Rails.application.routes.draw do
-  mod = 'docin'
-
-  ## admin
-  scope "#{Zomeki::ADMIN_URL_PREFIX}/#{mod}/c:concept", :module => "#{mod}/admin", :as => mod do
-    resources :imports,
-      controller: 'imports',
-      path: ':content/imports'
-    resources :contents,
-      :controller => 'admin/contents',
-      :path       => 'contents'
-
-    ## contents
-    resources :content_contents,
-      controller: 'content/contents'
+admin_scope 'docin/c:concept', module: 'docin/admin' do
+  scope ':content' do
+    resources :imports
     resources :content_settings,
       controller: 'content/settings',
-      path: ':content/content_settings',
       only: [:index, :show, :edit, :update]
+  end
 
+  namespace :content do
+    resources :imports,
+      only: [:show, :edit, :update]
   end
 end
